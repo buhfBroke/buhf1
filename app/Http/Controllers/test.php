0336323ea78433777bb5fe5995e03846df275ac1
buhfBroke <darenskiy.masik@mail.ru>
2022-12-01 05:12:45 -0800

    Начало работы

--- app/Http/Controllers/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-use App\Http\Controllers\Controller;
-class test extends Controller
-{
-    public function sum($num1,$num2){
-        return $num1 + $num2;
-    }
-}
